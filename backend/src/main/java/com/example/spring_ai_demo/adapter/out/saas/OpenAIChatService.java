62f312389cd5c9f0d3965669c160afcd922cb921
HORIE Tetsuya <horie-t@users.noreply.github.com>
2026-02-12 22:19:51 +0900

    第4章を実装する

--- backend/src/main/java/com/example/spring_ai_demo/adapter/out/saas/OpenAIChatService.java
+++ backend/src/main/java/com/example/spring_ai_demo/adapter/out/saas/OpenAIChatService.java
@@ -1,18 +1,25 @@
 package com.example.spring_ai_demo.adapter.out.saas;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OpenAIChatService {
     private final ChatClient chatClient;
+    private final ChatMemory chatMemory;
 
-    public OpenAIChatService(ChatClient.Builder builder) {
+    public OpenAIChatService(ChatClient.Builder builder, ChatMemory chatMemory) {
         this.chatClient = builder.build();
+        this.chatMemory = chatMemory;
     }
 
     public String withUserMessage(String userMessage) {
         return chatClient.prompt()
+                .advisors(new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .user(userMessage)
                 .call()
                 .content();
